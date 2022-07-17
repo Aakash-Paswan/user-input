5cc8bbebfceda88cfcd179eed1f29e254ed8da0d
AAKASH-BufferZero <kaakash97092@gmail.com>
2022-07-17 16:08:21 +0530

    added table design using tailwind

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,58 +1,85 @@
-import { Formik } from "formik"
-import { useState } from "react"
-
+import { Formik } from "formik";
+import { useState } from "react";
+import { Input } from "@material-tailwind/react";
 function Login() {
-    const [data, setData]=useState([])
+  const [data, setData] = useState([]);
   return (
     <div>
-        <Formik initialValues={
-            {
-                firstName:"",
-                lastName:"",
-                email:"",
-                address:"",
-            }
-        }
-        onSubmit={values=>{
-            console.log(values)
-            // const newData = {...values}
-            setData(prevData=>[...prevData, values])
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          address: "",
         }}
-        >
-       {({values, handleSubmit, handleChange})=>(
-         <form onSubmit={handleSubmit}>
-         <input type="text"  name="firstName" id="firstName" placeholder="First Name" onChange={handleChange}  value={values.fullName} />
-         <input type="text" name="lastName" id="lastName" placeholder="Last Name" onChange={handleChange}  value={values.lastName}/>
-         <input type="email" name="email" id="email" placeholder="Email" onChange={handleChange} value={values.email}/>
-         <input type="text" name="address" id="address" placeholder="Full Address" onChange={handleChange} value={values.address}/>
-         <button type="submit">Submit</button>
-
-     </form>
-       )}
-
-        </Formik>
-    <table border='1'>
-        <thead >
-            <tr>
-                <th>FirstName</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Address</th>
-            </tr>
+        onSubmit={(values) => {
+          console.log(values);
+          // const newData = {...values}
+          setData((prevData) => [...prevData, values]);
+        }}
+      >
+        {({ values, handleSubmit, handleChange }) => (
+          <form className="flex w-full items-end gap-4" onSubmit={handleSubmit}>
+            <Input
+              variant="outlined"
+              type="text"
+              name="firstName"
+              id="firstName"
+              onChange={handleChange}
+              value={values.fullName}
+            />
+            <Input
+              variant="outlined"
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="Last Name"
+              onChange={handleChange}
+              value={values.lastName}
+            />
+            <Input
+              variant="outlined"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={handleChange}
+              value={values.email}
+            />
+            <Input
+              variant="outlined"
+              type="text"
+              name="address"
+              id="address"
+              placeholder="Full Address"
+              onChange={handleChange}
+              value={values.address}
+            />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
+      <table className="min-w-full">
+        <thead className="border-b">
+          <tr>
+            <th>FirstName</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
         </thead>
         <tbody>
-           { data.map((x)=>(<tr>
-                <td>{x.firstName}</td>
-                <td>{x.lastName}</td>
-                <td>{x.email}</td>
-                <td>{x.address}</td>
-
-            </tr>))}
+          {data.map((x) => (
+            <tr>
+              <td>{x.firstName}</td>
+              <td>{x.lastName}</td>
+              <td>{x.email}</td>
+              <td>{x.address}</td>
+            </tr>
+          ))}
         </tbody>
-    </table>
-
-
+      </table>
     </div>
-  )
+  );
 }
-export default Login
+export default Login;
